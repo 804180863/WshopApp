9cd801a2b19dc225cd0d36e3c3c6968f9166d3df
wangxuelong <804180863@qq.com>
2019-02-23 11:22:59 +0800

    静态页面实现

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import app from './app'
 import router from './roter'
+import Header from './components/Header/Header.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// require styles
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper/* { default global options } */)
+Vue.component('Header', Header)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
